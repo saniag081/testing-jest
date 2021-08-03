3f24238554995ae5ce286f2a1c15e3d46df65429
saniag081 <sgonzalezhernandez081@gmail.com>
2021-08-02 22:09:13 -0500

    before and after at jest

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -17,3 +17,39 @@ test('verdadero', () => {
 	expect(true).toBeTruthy()
 })
 
+const reverseString = (str, callback) => {
+	callback(str.split("").reverse().join(""))
+}
+
+test('Probar Callback', () => {
+	reverseString('Hola', (str) => {
+		expect(str).toBe('aloH');
+	});
+})
+
+const reverseString2 = str => {
+	return new Promise((resolve, reject) => {
+		if (!str) {
+			reject(Error('Error'))
+		}
+		resolve(str.split("").reverse().join(""))
+	})
+}
+
+test('Probar una promesa', () => {
+	return reverseString2('Hola')
+		.then(str => {
+			expect(str).toBe('aloH')
+		});
+});
+
+test('Probar sync/wait', async () => {
+	const str = await reverseString2('Hola')
+	expect(str).toBe('aloH');
+});
+
+afterEach(() => console.log('Despues de cada prueba'));
+afterAll(() => console.log('Despues de todas las pruebas'))
+
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las peubas'))
